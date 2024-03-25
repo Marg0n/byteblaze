4cd745c4c6c086e6c67537861c51e67d53f8ae62
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-03-25 23:56:07 +0600

    fixed blogs page theme toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home';
-import Blogs from './pages/Blogs';
-import Bookmarks from './pages/Bookmarks';
-import './index.css';
-import MainLayout from './layouts/MainLayout';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Bookmarks from "./pages/Bookmarks";
+import "./index.css";
+import MainLayout from "./layouts/MainLayout";
+import Blog from "./pages/Blog";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
@@ -21,21 +18,26 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/blogs',
+        path: "/blogs",
         element: <Blogs />,
-        loader: () => fetch('https://dev.to/api/articles?per_page=20&top=7'),
+        loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
       },
       {
-        path: '/bookmarks',
+        path: "/blog/:id",
+        element: <Blog />,
+        loader: ({ params }) =>
+          fetch(`https://dev.to/api/articles/${params.id}`),
+      },
+      {
+        path: "/bookmarks",
         element: <Bookmarks />,
       },
-    ]
+    ],
   },
-  
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+
+const Blog = () => {
+  const blog = useLoaderData();
+
+  return (
+    <div>
+      <h1>{blog.title}</h1>
+    </div>
+  );
+};
+
+export default Blog;
